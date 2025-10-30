3da9ccb9c22dd2b646e5d619ddf678817e9afd7f
Kim Svahn <kim.svahn@msb.se>
2025-10-30 10:59:41 +0100

    .

--- install-sealedsecrets.sh
+++ install-sealedsecrets.sh
@@ -17,6 +17,8 @@ kubectl -n kube-system create secret tls sealed-secrets-custom-key \
 kubectl -n kube-system label secret sealed-secrets-custom-key \
   sealedsecrets.bitnami.com/sealed-secrets-key=active
 
+rm -f sealed-secrets.crt sealed-secrets.key
+
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update
 
@@ -28,4 +30,6 @@ helm install sealed-secrets -n kube-system \
 curl -LO "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
 tar -xvzf "kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz" kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
-kubeseal --version
+
+kubectl create secret generic dev1-db-pass --from-literal=username=dev1 --from-literal=password=dev123 --dry-run=client -o json | kubeseal --namespace=dev1 --format=yaml > app1/templates/SealedSecret.db-pass.yaml
+kubectl create secret generic dev2-db-pass --from-literal=username=dev2 --from-literal=password=dev123 --dry-run=client -o json | kubeseal --namespace=dev2 --format=yaml > app2/templates/SealedSecret.db-pass.yaml
