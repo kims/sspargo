27b106daca412db524c4c1dc9b0af92577d0e5ef
Kim Svahn <kim.svahn@msb.se>
2025-10-30 11:33:38 +0100

    .

--- install-sealedsecrets.sh
+++ install-sealedsecrets.sh
@@ -27,10 +27,7 @@ helm install sealed-secrets -n kube-system \
   --set-string fullnameOverride=sealed-secrets-controller \
   sealed-secrets/sealed-secrets
 
-curl -LO "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
-tar -xvzf "kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz" kubeseal
-sudo install -m 755 kubeseal /usr/local/bin/kubeseal
-rm -f kubeseal kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz
+K_VERSION=$(curl -sL https://api.github.com/repos/bitnami/sealed-secrets/releases/latest | grep '"tag_name"' | awk -F': ' '{print $2}' | tr -d '",'); [ -x /usr/local/bin/kubeseal ] || { curl -sLO "https://github.com/bitnami/sealed-secrets/releases/download/${K_VERSION}/kubeseal-linux-amd64" && sudo install -m 755 kubeseal-linux-amd64 /usr/local/bin/kubeseal && rm kubeseal-linux-amd64; } && kubeseal version
 
 kubectl create secret generic dev1-db-pass --from-literal=username=dev1 --from-literal=password=dev123 --dry-run=client -o json | kubeseal --namespace=dev1 --format=yaml > app1/templates/SealedSecret.db-pass.yaml
 kubectl create secret generic dev2-db-pass --from-literal=username=dev2 --from-literal=password=dev123 --dry-run=client -o json | kubeseal --namespace=dev2 --format=yaml > app2/templates/SealedSecret.db-pass.yaml
